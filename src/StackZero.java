13dfb7bf7bdb08d90e90e3b29a28d69c94fc257e
Jy_Park <nkooog@naver.com>
2024-02-15 16:49:01 +0900

    ㄴã10773

---
+++ src/StackZero.java
@@ -0,0 +1,32 @@
+public class StackZero {
+
+	static int top;
+	static int size;
+	static int[] array;
+
+	public static void main(String[] args) {
+
+	}
+
+	public static <E> void push (E item) {
+
+	}
+
+	// Stack의 top에 있는 item을 삭제하고 해당 item을 반환
+	public static <E> int pop () {
+		return 0;
+	}
+
+	// Stack의 top에 있는 item을 삭제하지 않고 해당 item을 반환
+	public static <E> int peek () {
+		return 0;
+	}
+
+	// Stack이 비워있으면 true, 아니면 false를 반환
+	public static boolean empty() {
+		return array.length > 0  ? true : false;
+	}
+
+
+
+}
