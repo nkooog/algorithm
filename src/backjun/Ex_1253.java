bb912d59fe0d97a448be521e92fc6f96870885d4
Jy_Park <nkooog@naver.com>
2025-01-21 16:56:19 +0900

    투포인터 1253번(진행중)

---
+++ src/backjun/Ex_1253.java
@@ -0,0 +1,54 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex_1253 {
+
+	public static void main(String[] args) throws Exception{
+
+		// A[left] + A[right] > K = j--;
+		// A[left] + A[right] < K = i++;
+		// A[left] + A[right] == K = i++; j--; count++;
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(reader.readLine());
+		int count = 0;
+		long[] arr = new long[N];
+		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+		for(int i=0; i<N; i++) {
+			arr[i] = Long.parseLong(tokenizer.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		for(int K = 0; K < N; K++) {
+			long find = arr[K];
+			int left =0, right= (arr.length -1), sum = 0;
+
+			while(left < right) {
+				if(arr[left] + arr[right] == find) {
+					if( left != right && right != left) {
+						count++; left++; right--;
+						break;
+					}else if(left == right) {
+						left++;
+					}else{
+						right--;
+					}
+
+				}else if(arr[left] + arr[right] < find) {
+					left++;
+				}else{
+					right--;
+				}
+			}
+		}
+		System.out.println(count);
+		reader.close();
+	}
+}
