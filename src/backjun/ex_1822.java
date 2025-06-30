241a2aef79d20c05e5ea9c1638751cf46b3fd816
jypark <nkooog.dev@gmail.com>
2025-06-30 14:35:27 +0900

    2025.06.27(차집합)

--- src/backjun/ex_1822.java
+++ src/backjun/ex_1822.java
@@ -11,33 +11,58 @@ public class ex_1822 {
 	static StringBuilder builder = new StringBuilder();
 	public static void main(String[] args) throws Exception{
 
-		ArrayList<String> list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>();
 
 		String N = reader.readLine();
 
-		String[] s1 = reader.readLine().split(" ");
-		String[] s2 = reader.readLine().split(" ");
+		String s1 = reader.readLine();
+		String s2 = reader.readLine();
 
-		Set A = new LinkedHashSet(Arrays.asList(s1));
-		Set B = new LinkedHashSet(Arrays.asList(s2));
+		Set A = new LinkedHashSet();
+		Set B = new LinkedHashSet();
 
-		Iterator<String> iter = A.iterator();
+		A = getSetData(A, s1);
+		B = getSetData(B, s2);
+
+		Iterator<Integer> iter = A.iterator();
 
 		while(iter.hasNext()) {
-			String num = iter.next();
-			if(!B.contains(num) && !"".equals(num)) {
+			int num = iter.next();
+			if(!B.contains(num)) {
 				list.add(num);
 			}
 		}
 
 		Collections.sort(list);
 
-		for(String s: list) {
+		for(int s: list) {
 			builder.append(s).append(" ");
 		}
 
 		System.out.println(list.size());
 		System.out.println(builder.toString());
+	}
+
+	public static Set<Integer> getSetData(Set<Integer> set, String s) {
+		for(int a : getArrays(s)) {
+			set.add(a);
+		}
+		return set;
+	}
+
+	public static int[] getArrays(String s) {
+
+		StringTokenizer st = new StringTokenizer(s, " ");
+
+		int[] arr = new int[st.countTokens()];
+
+		int i=0;
+		while(st.hasMoreTokens()) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			i++;
+		}
+
+		return arr;
 	}
 
 }
